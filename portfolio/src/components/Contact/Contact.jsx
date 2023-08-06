983ab38edbe73011c1317b76f8a6e282d5134759
A1Kumari <aryakumari3953@gmail.com>
2023-08-06 17:55:20 +0530

    contact section

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { AiFillLinkedin } from 'react-icons/ai';
 import './Contact.css';
 
 const Contact = () => {
   return (
+    <>
+    <h2 className="contact heading">Contact Me</h2>
     <section className="contact-section">
       <div className="contact-section-container">
-        <h2 className="">Contact Me</h2>
-        <div className="">
-          <div className="">
+        <div className="Contact">
+          
+          <div className="contact-con glass-morphism-card">
             <h3 className="">Get in Touch</h3>
             <form>
               <div className="form-group">
@@ -22,18 +27,24 @@ const Contact = () => {
                 <label htmlFor="message">Message</label>
                 <textarea id="message" name="message" className="input" rows="4" required />
               </div>
-              <button type="submit" className="btn">Send Message</button>
+              <button className="cta-button">View Projects</button>
             </form>
           </div>
-          <div className="glass-morphism-card">
-            <h3 className="">About Me</h3>
-            <p className="">
+          <div className="about-part">
+            <h3 className="Aboutheading">About Me</h3>
+            <p className="about-para">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu facilisis sodales, dui odio ultricies justo, in cursus risus purus at nisi.
             </p>
+            <div className="social-links-contact">
+        <a href="#" className="social-link"><AiOutlineInstagram/></a>
+        <a href="#" className="social-link"><AiOutlineGithub/></a>
+        <a href="#" className="social-link"><AiFillLinkedin/></a>
+      </div>
           </div>
         </div>
       </div>
     </section>
+    </>
   );
 };
 
